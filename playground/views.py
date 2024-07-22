e4cb32ef7560792c5963aa24f6647be40243b4c7
Pinku Deb Nath <prantoran@gmail.com>
2024-07-21 18:55:25 -0700

    Filtering using F objects

--- playground/views.py
+++ playground/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.core.exceptions import ObjectDoesNotExist
 from django.http import HttpResponse
-from django.db.models import Q
+from django.db.models import Q, F
 from store.models import Product
 
 # Create your views here.
@@ -21,13 +21,18 @@ def say_hello(request):
 
     # exists = Product.objects.filter(pk=0).exists() # None if does not exists
 
-    # Products: inventory < 10 AND price < 20
+    # # Products: inventory < 10 AND price < 20
     # queryset = Product.objects.filter(inventory__lt=10, unit_price__lt=20)
-    queryset = Product.objects.filter(inventory__lt=10).filter(unit_price__lt=20)
+    # queryset = Product.objects.filter(inventory__lt=10).filter(unit_price__lt=20)
 
-    # Products: inventory < 10 AND price < 20
-    queryset = Product.objects.filter(
-        Q(inventory__lt=10) | ~Q(unit_price__lt=20))
+    # # Products: inventory < 10 AND price < 20
+    # queryset = Product.objects.filter(
+    #     Q(inventory__lt=10) | ~Q(unit_price__lt=20))
+
+    # # Products: inventory = price
+    # queryset = Product.objects.filter(inventory=F('unit_price'))
+    # # Compare with a field in a related table
+    # queryset = Product.objects.filter(inventory=F('collection__id'))
 
     # return HttpResponse('Hello World')
     return render(request, 'hello.html', { 'name': 'Goku', 'products': list(queryset)})
