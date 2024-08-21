10354deb1a8ee1180b24cb75cdc5e009b2e4fc7b
Pinku Deb Nath <prantoran@gmail.com>
2024-08-20 19:52:23 -0700

    Deferring fields

--- playground/views.py
+++ playground/views.py
@@ -34,10 +34,11 @@ def say_hello(request):
     # # Compare with a field in a related table
     # queryset = Product.objects.filter(inventory=F('collection__id'))
 
+# ----------------
     # # sorting
     # queryset = Product.objects.order_by('unit_price', '-title').reverse()
     # product = Product.objects.order_by('unit_price')[0]
-    product_cheapest = Product.objects.earliest('unit_price')
+    # product_cheapest = Product.objects.earliest('unit_price')
     # product_priciest = Product.objects.latest('unit_price')
 
     # # limiting results
@@ -45,10 +46,16 @@ def say_hello(request):
 
     # # select
     # queryset = Product.objects.values('id', 'title', 'unit_price', 'collection__title') # returns array of dicts
-    queryset = Product.objects.values_list('id', 'title', 'unit_price', 'collection__title')
+    # queryset = Product.objects.values_list('id', 'title', 'unit_price', 'collection__title')
 
-    queryset = Product.objects.filter(
-        id__in=OrderItem.objects.values('product__id').distinct().order_by('title')
-    )
+    # queryset = Product.objects.filter(
+    #     id__in=OrderItem.objects.values('product__id').distinct().order_by('title')
+    # )
     # return HttpResponse('Hello World')
-    return render(request, 'hello.html', { 'name': 'Goku', 'products': list(queryset), 'cheapest': product_cheapest })
+    # return render(request, 'hello.html', { 'name': 'Goku', 'products': list(queryset), 'cheapest': product_cheapest })
+# ----------------
+
+    # queryset = Product.objects.only('id', 'title')
+    queryset = Product.objects.defer('description') # defer the loading of certain fields later
+    return render(request, 'hello.html', { 'name': 'Goku', 'products': list(queryset) })
+    
