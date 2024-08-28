cd9a37592e648e8e33cb07aa7cf7dc09361819c2
Pinku Deb Nath <prantoran@gmail.com>
2024-08-27 22:43:54 -0700

    Create link to the orders for each customer in the admin panel

--- store/admin.py
+++ store/admin.py
@@ -28,11 +28,25 @@ class ProductAdmin(admin.ModelAdmin):
 
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
-    list_display = ['first_name', 'last_name', 'membership']
+    list_display = ['first_name', 'last_name', 'membership', 'orders_count']
     list_editable = ['membership']
     ordering = ['first_name', 'last_name']
     list_per_page = 10
 
+    @admin.display(ordering='orders_count')
+    def orders_count(self, customer):
+        url = (
+            reverse('admin:store_order_changelist') 
+            + '?'
+            + urlencode({
+                'customer__id': str(customer.id)
+            }))
+        return format_html('<a href="{}">{}</a>', url, customer.orders_count)
+
+    def get_queryset(self, request: HttpRequest) -> QuerySet[Any]:
+        return super().get_queryset(request).annotate(
+            orders_count=Count('order')
+        )
 
 # admin.site.register(models.Collection)
 @admin.register(models.Collection)
